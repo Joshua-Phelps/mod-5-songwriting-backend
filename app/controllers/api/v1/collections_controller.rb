1182c52c631043a5695c83759d0201fef12264dc
jugtuttle <jugtuttle@gmail.com>
2020-02-24 22:05:49 -0800

    edit song title works

--- app/controllers/api/v1/collections_controller.rb
+++ app/controllers/api/v1/collections_controller.rb
@@ -1,9 +1,8 @@
 class Api::V1::CollectionsController < ApplicationController
     def create
-        byebug 
         user = User.find(params[:user_id])
         collection = Collection.new(collection_name: params[:collection_name], user_id: params[:user_id])
         collection.save
-        render json: user 
+        render json: { id: user.id, username: user.username, collections: user.collections, songs: user.songs } 
     end
 end
--- app/controllers/api/v1/songs_controller.rb
+++ app/controllers/api/v1/songs_controller.rb
@@ -10,9 +10,21 @@ class Api::V1::SongsController < ApplicationController
         user = collection.user
         song = Song.new(collection_id: params[:collection_id], title: params[:song_title])
         if song.save
-            render json: user 
+            render json: { id: user.id, username: user.username, collections: user.collections, songs: user.songs }
         else 
             # needs error handling 
+        end 
+    end
+
+    def update
+        song = Song.find(params[:id])
+        song.title = params[:song_title]
+        song.collection_id = params[:collection_id]
+        user = song.collection.user
+        if song.save
+            render json: { id: user.id, username: user.username, collections: user.collections, songs: user.songs }
+        else
+            ## error handle 
         end 
     end
 end
