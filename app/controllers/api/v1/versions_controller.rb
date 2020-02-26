54d84838c1d2de899f46f4be9f34316a3d764eb7
jugtuttle <jugtuttle@gmail.com>
2020-02-26 11:46:07 -0800

    can delete/edit a version

--- app/controllers/api/v1/versions_controller.rb
+++ app/controllers/api/v1/versions_controller.rb
@@ -3,8 +3,31 @@ class Api::V1::VersionsController < ApplicationController
     def create
         version = Version.create(title: params[:title], song_id: params[:id])
         version.recording = params[:recording]
-        version.save
-        song = version.song
+        if version.save
+            song = version.song
+            sorted_versions = song.versions.order({ created_at: :desc }) 
+            render json: sorted_versions
+        else 
+            ##error handle 
+        end 
+    end
+
+    def update
+        version = Version.find(params[:id])
+        version.title = params[:title]
+        if version.save
+            song = version.song
+            sorted_versions = song.versions.order({ created_at: :desc }) 
+            render json: sorted_versions
+        else
+            ##error handle  
+        end 
+    end
+
+    def destroy
+        version = Version.find(params[:id])
+        song = version.song 
+        version.destroy
         sorted_versions = song.versions.order({ created_at: :desc }) 
         render json: sorted_versions
     end
