9c23274cf9fbec9fcbfaeb8f7fef5ff6bb1b977b
jugtuttle <jugtuttle@gmail.com>
2020-03-12 16:45:08 -0700

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,7 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
+  config.allowed_cors_origins = 'https://song-control-client.herokuapp.com/'
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
