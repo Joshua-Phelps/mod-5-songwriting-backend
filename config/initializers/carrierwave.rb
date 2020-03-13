cd431b55eec86935601600d5a597dbfcc3fb4ad5
jugtuttle <jugtuttle@gmail.com>
2020-03-12 17:53:02 -0700

    testing

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,4 +1,5 @@
 CarrierWave.configure do |config|
+    config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider:              'AWS',                        # required
       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],                         # required unless using use_iam_profile
